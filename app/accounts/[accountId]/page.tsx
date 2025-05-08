803e0fcf37580c35a19075cf0a027abbff297da6
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2025-05-08 18:21:39 +0300

    test

--- app/accounts/[accountId]/page.tsx
+++ app/accounts/[accountId]/page.tsx
@@ -19,8 +19,8 @@ type AccountPageProps = {
 async function getAccountData(accountId: string): Promise<AccountData | null> {
   const reqHeaders = await headers();
   const host = reqHeaders.get('host');
-  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
-  const baseUrl = `${protocol}://${host}`;
+  const baseUrl = `https://${host}`;
+
   
   try {
     const res = await fetch(`${baseUrl}/api/accounts/${encodeURIComponent(accountId)}`, {
